4b1f3e5ea9b875a6b10a2527d403617a1620b150
Matan <matanbitton2@gmail.com>
2022-08-29 15:28:21 +0300

    initial setup webpack and tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: ["./dist/*.html"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: path.join(__dirname, "./dist/index.html"),
+      inject: false,
+    }),
+  ],
+
+  devtool: "inline-source-map",
+  devServer: {
+    port: 3000,
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+
+            plugins: ["@babel/plugin-transform-arrow-functions"],
+          },
+        },
+      },
+    ],
+  },
+};
