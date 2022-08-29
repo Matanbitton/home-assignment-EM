388706025f7dd6ec9e445f737fd533e494d3ddd3
Matan <matanbitton2@gmail.com>
2022-08-29 22:41:26 +0300

    finish mobile tv section

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+import Icon from "./tv.jpg";
+
+const component = (() => {
+  const tvImg = new Image();
+  tvImg.src = Icon;
+  const tvHtml = document.querySelector(".tv-icon");
+  tvHtml.append(tvImg);
+})();
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   purge: ["./dist/*.html"],
 
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: { min: "0px", max: "720px" },
+
+        tablet: { min: "720px", max: "1074px" },
+
+        desktop: { min: "1074px", max: "1920px" },
+      },
+      backgroundImage: {
+        "tv-icon": "url('./tv.jpg')",
+      },
+    },
   },
   plugins: [],
 };
