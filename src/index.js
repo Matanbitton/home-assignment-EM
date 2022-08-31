24d786127a1c6533cda372a878916c2da61c4e3a
Matan <matanbitton2@gmail.com>
2022-09-01 00:05:18 +0300

    finish desktop responsive design

--- src/index.js
+++ src/index.js
@@ -6,22 +6,4 @@ const component = (() => {
   tvImg.src = Icon;
   const tvHtml = document.querySelector(".tv-icon");
   tvHtml.append(tvImg);
-
-  /* const tvDescription = document.querySelector(".product-description");
-  const productRating = document.querySelector(".product-rating");
-  const productContainer = document.querySelector(".product-container");
-
-  console.log(tvDescription, productRating, productContainer);
-
-  let productContainerArr = Array.from(productContainer);
-  console.log(productContainerArr);
-
-  console.log(productContainer.style.width);
-
-  productContainer.removeChild(productRating);
-  productContainerArr = productContainerArr.filter(
-    (child) => !child.classList.includes("product-rating")
-  );
-
-  console.log(productContainerArr); */
 })();
